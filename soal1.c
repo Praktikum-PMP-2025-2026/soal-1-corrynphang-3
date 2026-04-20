572e3bea2473de8d00327ab358c8ba168ff5376c
Beatrice Corryn Pangestu <124460876+corrynphang@users.noreply.github.com>
2026-04-20 09:07:04 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,55 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 02 - Data Manipulation and External Files
+ *   Hari dan Tanggal    : Senin, 20 April 2026
+ *   Nama (NIM)          : Beatrice Corryn Pangestu (13224038)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Di sebuah stasiun pengamatan cuaca ekstrem, deretan sensor badai mengirimkan data intensitas angin 
+ * secara berkala. Sayangnya, beberapa pembacaan hilang akibat gangguan transmisi dan ditandai 
+sebagai data kosong. Sebelum data tersebut dipakai oleh tim analisis, sistem harus terlebih dahulu 
+memulihkan nilai yang hilang berdasarkan pembacaan valid terdekat. Setelah seluruh data 
+dipulihkan, tim ingin mengetahui rentang waktu pengamatan paling berbahaya, yaitu segmen kontigu 
+dengan total intensitas terbesar.  
+Pulihkan elemen array yang hilang lalu cari jumlah subarray maksimum.
+ * 
+ */
 
-int main(void) {
-    return 0;
-}
+ #include <stdio.h>
+ void cek(int *angka,int jumlah){
+    int max=0;
+    if (*angka == -1){
+        int i=1;
+        while (*angka == -1){
+            if (*(angka+i) != -1){
+                *angka= *(angka+i)/2;
+            }
+            i++;
+        }
+    }
+    for (int i=1; i<jumlah;i++){
+        int n=i+1;
+        while (*(angka+i)== -1){
+            if (*(angka+n)!= -1){
+                *(angka+i)= (*(angka+i-1)+*(angka+n))/2;
+            }
+            n++;
+        }
+    }
+    for (int i=0; i<jumlah;i++){
+        max+=*(angka+i);
+    }
+    printf("\nRECOVERED ");
+    for (int i=0; i<jumlah;i++){
+        printf("%d ",*(angka+i));
+    }
+    printf("\nMAX_SUM %d",max);
+ }
+ int main(){
+    int N;
+    scanf("%d",&N);
+    int arr[N];
+    int *ptr= arr;
+    for (int i=0; i<N;i++){
+        scanf("%d",(ptr+i));
+    }
+    cek(ptr,N);
+ }
